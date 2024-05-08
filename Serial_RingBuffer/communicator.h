34f7b42d62f15b3fe7454c0ca4c317e518b32593
P05eidon <35781548+P05eidon@users.noreply.github.com>
2024-05-07 23:02:13 -0400

    Add files via upload

---
+++ Serial_RingBuffer/communicator.h
@@ -0,0 +1,17 @@
+/*
+ * communicator.h
+ *
+ *  Created on: May 7, 2024
+ *      Author: p05ei
+ */
+#ifndef COMMUNICATOR_H_
+#define COMMUNICATOR_H_
+
+#include "serial.h"
+#include "ring_buffer8.h"
+
+void MessageReceiver();
+
+
+
+#endif /* COMMUNICATOR_H_ */
